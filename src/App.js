62fa17686d5376fc7ef2ca6de79f36998ab2f298
SporeFinance <u511.511.511u@gmail.com>
2021-03-19 20:43:00 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Header from './Header'
+import Informations from './Informations'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Informations />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
